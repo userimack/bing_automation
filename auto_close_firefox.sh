d0307f1b070bd07a6a5093a6c5e944d67093302a
userimack <mahendra.k12@gmail.com>
2016-05-04 23:07:33 +0530

    Added new code to automatically close firefox after search

---
+++ auto_close_firefox.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+count=0
+url='https://www.bing.com/search?q='
+for i in `cat profiles$1`; do
+    profile="-P $i"
+    firefox $profile https://www.bing.com/search?q=mahendra+yadav+userimack &
+    sleep 2
+
+    while IFS= read -r i; do 
+        # if (( $count==0 ))
+        # then
+        #   p="$profile --new-window"
+         
+        # else
+        #   p="$profile  --new-tab"
+        # fi
+        p="$profile  --new-tab"
+        echo "$(expr $count + 1 ) $p '$url$i'"
+        $(firefox $p "$url$i" )
+        sleep 4
+        
+
+    done < query
+    sleep 20
+    wmctrl -ic "$(wmctrl -lp | grep "$(pgrep firefox)" | tail -1 | awk '{ print $1 }')"    
+    ((count++ ))
+done
