0ea4485e2d3dc069e75bed91cf77e43562617627
userimack <mahendra.k12@gmail.com>
2016-05-05 11:11:27 +0530

    Modified file

--- check_firefox.sh
+++ check_firefox.sh
@@ -4,8 +4,14 @@ url="https://www.bing.com/rewardsintl/dashboard"
 for i in `cat profiles$1`; do
 	profile="-P $i"
 	echo "running user --- $i  site --- '$url'"
-	firefox $profile &
+	firefox $profile $url &
 	sleep 5
-	firefox $profile $url
-	sleep 5
+	#firefox $profile $url
+    ((count++))
+	if [ $count -eq 5 ] 
+        then
+        count=0
+        echo "Proceed..... : Enter any key Continue : "
+        read
+    fi
 done
