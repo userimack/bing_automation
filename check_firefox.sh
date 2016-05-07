376e56739e9144b28ff25b66be4ab0a04291453d
userimack <mahendra.k12@gmail.com>
2016-05-08 01:03:23 +0530

    modified code

--- check_firefox.sh
+++ check_firefox.sh
@@ -4,7 +4,7 @@ url="https://www.bing.com/rewardsintl/dashboard"
 for i in `cat profiles$1`; do
 	profile="-P $i"
 	echo "running user --- $i  site --- '$url'"
-	firefox $profile $url &
+	nohup firefox $profile $url &
 	sleep 5
 	#firefox $profile $url
     ((count++))
