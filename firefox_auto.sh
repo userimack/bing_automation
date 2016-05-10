fb096f5ebb3c71c3526d1b6403d13ee0ad3ae691
userimack <mahendra.k12@gmail.com>
2016-05-10 22:17:09 +0530

    Modified some code

--- firefox_auto.sh
+++ firefox_auto.sh
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env bash
 
 count=0
@@ -5,7 +6,7 @@ url='https://www.bing.com/search?q='
 for i in `cat profiles$1`; do
 	profile="-P $i"
 	firefox $profile https://www.bing.com/search?q=mahendra+yadav+userimack &
-	sleep 20
+	sleep 10
 
 	while IFS= read -r i; do 
 		# if (( $count==0 ))
@@ -18,9 +19,10 @@ for i in `cat profiles$1`; do
 		p="$profile  --new-tab"
 		echo "$(expr $count + 1 ) $p '$url$i'"
 		$(firefox $p "$url$i" )
-		sleep 2
+		sleep 5
 		
 
 	done < query
+	#wmctrl -ic "$(wmctrl -lp | grep "$(pgrep firefox)" | tail -1 | awk '{ print $1 }')"
 	((count++ ))
 done
